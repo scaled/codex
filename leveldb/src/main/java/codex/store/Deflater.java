594d6b950667bedba66355aecebe27d1261ca507
Michael Bayne <mdb@samskivert.com>
2014-05-30 14:25:04 -0700

    Some work on a LevelDB-based persistent store.

---
+++ leveldb/src/main/java/codex/store/Deflater.java
@@ -0,0 +1,110 @@
+//
+// Codex - a framework for grokking code
+// http://github.com/scaled/codex/blob/master/LICENSE
+
+package codex.store;
+
+import codex.model.*;
+import java.nio.ByteBuffer;
+import java.util.List;
+
+public class Deflater extends Flater {
+
+  public Deflater () {
+    this(128);
+  }
+
+  public Deflater (int expectedSize) {
+    _data = new byte[expectedSize];
+    _buf = ByteBuffer.wrap(_data);
+  }
+
+  public byte[] bytes () {
+    return _data;
+  }
+
+  public Deflater putBoolean (boolean value) {
+    prepPut(1).put(value ? TRUE : FALSE);
+    return this;
+  }
+
+  public Deflater putInt (int value) {
+    prepPut(4).putInt(value);
+    return this;
+  }
+
+  public Deflater putString (String value) {
+    byte[] data = toBytes(value);
+    prepPut(data.length+4).putInt(data.length).put(data);
+    return this;
+  }
+
+  public Deflater putDef (Def def) {
+    byte[] kind = toBytes(def.kind.name());
+    byte[] name = toBytes(def.name);
+    int size = (4 /*id*/ + 4 /*outerId*/ + 4+kind.length + 1 /*exported*/ + 4+name.length +
+                4 /*offset*/);
+    prepPut(size).putInt(def.id).putInt(def.outerId).putInt(kind.length).put(kind).
+      put(def.exported ? TRUE : FALSE).putInt(name.length).put(name).putInt(def.offset);
+    return this;
+  }
+
+  public Deflater putDefs (List<Def> defs) {
+    putInt(defs.size());
+    for (Def def : defs) putDef(def);
+    return this;
+  }
+
+  public Deflater putRef (Ref ref) {
+    if (ref instanceof Ref.Local) {
+      putBoolean(true);
+      putInt(((Ref.Local)ref).id);
+    } else {
+      putBoolean(false);
+      putString(ref.toString());
+    }
+    return this;
+  }
+
+  public Deflater putUse (Use use) {
+    putRef(use.ref);
+    byte[] refKind = toBytes(use.refKind.name());
+    int size = (4+refKind.length + 4 /*offset*/ + 4 /*length*/);
+    prepPut(size).putInt(refKind.length).put(refKind).putInt(use.offset).putInt(use.length);
+    return this;
+  }
+
+  // public List<Use> getUses () {
+  //   int useCount = getInt();
+  //   List<Use> uses = Lists.newArrayListWithExpectedSize(useCount);
+  //   for (int ii = 0; ii < useCount; ii++) uses.add(getUse());
+  //   return uses;
+  // }
+
+  // public Sig getSig () {
+  //   return new Sig(getString() /*text*/, getDefs(), getUses());
+  // }
+
+  // public Doc getDoc () {
+  //   return new Doc(getInt() /*offset*/, getInt() /*length*/, getUses());
+  // }
+
+  protected ByteBuffer prepPut (int bytes) {
+    int pos = _buf.position();
+    int need = pos + bytes;
+    if (need <= _data.length) return _buf;
+    else {
+      int nsize = ((need >> 7) + 1) << 7 + 128; // round up to mult of 128 then add 128
+      byte[] ndata = new byte[nsize];
+      System.arraycopy(_data, 0, ndata, 0, _data.length);
+      ByteBuffer buf = ByteBuffer.wrap(ndata);
+      buf.position(pos);
+      _data = ndata;
+      _buf = buf;
+      return buf;
+    }
+  }
+
+  private byte[] _data;
+  private ByteBuffer _buf;
+}
---
+++ leveldb/src/main/java/codex/store/Inflater.java
@@ -0,0 +1,74 @@
+//
+// Codex - a framework for grokking code
+// http://github.com/scaled/codex/blob/master/LICENSE
+
+package codex.store;
+
+import codex.model.*;
+import com.google.common.collect.Lists;
+import java.nio.ByteBuffer;
+import java.util.List;
+
+/**
+ * Inflates bytes into objects.
+ */
+public class Inflater extends Flater {
+
+  public Inflater (byte[] data) {
+    _data = data;
+    _buf = ByteBuffer.wrap(data);
+  }
+
+  public boolean getBoolean () {
+    return _buf.get() == TRUE;
+  }
+
+  public int getInt () {
+    return _buf.getInt();
+  }
+
+  public String getString () {
+    int length = getInt();
+    return fromBytes(_data, _buf.position(), length);
+  }
+
+  public Def getDef () {
+    return new Def(getInt() /*id*/, getInt() /*outerId*/, Kind.valueOf(getString()),
+                   getBoolean() /*exported*/, getString() /*name*/, getInt() /*offset*/);
+  }
+
+  public List<Def> getDefs () {
+    int defCount = getInt();
+    List<Def> defs = Lists.newArrayListWithExpectedSize(defCount);
+    for (int ii = 0; ii < defCount; ii++) defs.add(getDef());
+    return defs;
+  }
+
+  public Ref getRef () {
+    boolean local = getBoolean();
+    if (local) return Ref.local(getInt());
+    else return Ref.Global.fromString(getString());
+  }
+
+  public Use getUse () {
+    return new Use(getRef(), Kind.valueOf(getString()), getInt() /*offset*/, getInt() /*length*/);
+  }
+
+  public List<Use> getUses () {
+    int useCount = getInt();
+    List<Use> uses = Lists.newArrayListWithExpectedSize(useCount);
+    for (int ii = 0; ii < useCount; ii++) uses.add(getUse());
+    return uses;
+  }
+
+  public Sig getSig () {
+    return new Sig(getString() /*text*/, getDefs(), getUses());
+  }
+
+  public Doc getDoc () {
+    return new Doc(getInt() /*offset*/, getInt() /*length*/, getUses());
+  }
+
+  private final byte[] _data;
+  private final ByteBuffer _buf;
+}
